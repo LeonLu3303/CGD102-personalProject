4de3925e68bc5c8d705922dee84749ae969f6426
LeonLu3303 <leon.lu1103@gmail.com>
2022-07-24 12:15:18 +0800

    menu-03-

---
+++ app/menu.js
@@ -0,0 +1,20 @@
+//===第一區
+function fisrtBlock() {}
+document
+  .getElementById('first-latte')
+  .addEventListener('click', fisrtBlock, false);
+//===第二區
+function secondBlock() {}
+document
+  .getElementById('second-latte')
+  .addEventListener('click', secondBlock, false);
+//===第三區
+function thirdBlock() {}
+document
+  .getElementById('third-latte')
+  .addEventListener('click', thirdBlock, false);
+//===第四區
+function fourBlock() {}
+document
+  .getElementById('four-latte')
+  .addEventListener('click', fourBlock, false);
